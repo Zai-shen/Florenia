50230812fa4a1ac227b4060d4162a249763193e1
Miran Jank <ma0069@technikum-wien.at>
2023-02-18 22:19:12 +0100

    Added variety to all spawned Trees

---
+++ Assets/Scripts/Florenia/Utility/TreeTransformationRule.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using DungeonArchitect;
+
+namespace Florenia.Utility
+{
+    public class TreeTransformationRule : TransformationRule {
+        public float angleVariation = 20;
+        public float positionVariation = 0.1f;
+
+        public override void GetTransform(PropSocket socket, DungeonModel model, Matrix4x4 propTransform, System.Random random, out Vector3 outPosition, out Quaternion outRotation, out Vector3 outScale) {
+            base.GetTransform(socket, model, propTransform, random, out outPosition, out outRotation, out outScale);
+
+            var randomVal = random.value() * 2 - 1;
+            var angle = randomVal * angleVariation;
+            var rotation = Quaternion.Euler(0, angle, 0);
+            outRotation = rotation;
+            outPosition += new Vector3((random.value() * 2 - 1) * positionVariation,0,0);
+            outScale -= new Vector3(randomVal > 0 ? 0 : outScale.x * 2, 0, 0);
+        }
+    }
+
+}
